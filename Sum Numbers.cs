75d6dd9d4fd85f237f9954dd45f56728cd3d3d83
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-01-29 00:19:52 +0200

    Add files via upload

---
+++ Sum Numbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Action<int []> pointToPrintCount = PrintCount;
+            pointToPrintCount(array);
+            
+            Action<int []> pointToPrintSum = PrintSum;
+            pointToPrintSum(array);
+            
+        }
+        static void PrintCount(int[] array)
+        {
+            Console.WriteLine(array.Length);
+        }
+        static void PrintSum(int[] array)
+        {
+            Console.WriteLine(array.Sum());
+        }
+    }
+}
+
